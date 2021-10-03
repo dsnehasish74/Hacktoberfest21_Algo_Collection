c85d0786a66ff93fdc5b9c9b9b91a2e7b93bb9ba
Sanskruti Kanphade <sanskruti.kn@gmail.com>
2021-10-03 23:45:54 +0530

    Selection sort in python

---
+++ Python/Selection_sort.py
@@ -0,0 +1,20 @@
+def selectionSort(itemsList):
+   n = len(itemsList)
+   for i in range(n - 1):
+      minValueIndex = i
+
+      for j in range(i + 1, n):
+         if itemsList[j] < itemsList[minValueIndex]:
+            minValueIndex = j
+
+      if minValueIndex != i:
+         temp = itemsList[i]
+         itemsList[i] = itemsList[minValueIndex]
+         itemsList[minValueIndex] = temp
+
+   return itemsList
+
+
+el = [21, 6, 9, 33, 3]
+
+print(selectionSort(el))
