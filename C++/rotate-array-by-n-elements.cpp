8c6dddc4c83baf4fb56da25141527a47485e466a
Afran <69106645+AfranUsmani@users.noreply.github.com>
2021-10-09 21:01:26 +0530

    Update rotate-array-by-n-elements.cpp

--- C++/rotate-array-by-n-elements.cpp
+++ C++/rotate-array-by-n-elements.cpp
@@ -7,8 +7,7 @@ class Solution{
     //Function to rotate an array by d elements in counter-clockwise direction. 
     
     void rotateArr(int arr[], int d, int n){
-        // code here
-        //d=d%n;
+
         reverse(arr, arr+n);
         reverse(arr, arr+n-d);
         reverse(arr+n-d, arr+n);
